Bugfix: Correct computation of standard deviation

modeltrees/criteria.py
@@ -459,8 +459,8 @@ class ZRenormGradientSplitCriterion(GradientSplitCriterion):
         X2cs_r = X2cs_l[-1:, :] - X2cs_l
 
         # Compute standard deviation of left and right side for all splits
-        sigma_l = np.sqrt(np.maximum(X2cs_l[splits - 1, :] / (n_l - 1) - np.power(mu_l, 2), _EPS ** 2))
-        sigma_r = np.sqrt(np.maximum(X2cs_r[splits - 1, :] / (n_r - 1) - np.power(mu_r, 2), _EPS ** 2))
+        sigma_l = np.sqrt(np.maximum( (X2cs_l[splits - 1, :] - np.power(mu_l, 2) * n_l) / (n_l - 1), _EPS ** 2))
+        sigma_r = np.sqrt(np.maximum( (X2cs_r[splits - 1, :] - np.power(mu_r, 2) * n_r) / (n_r - 1), _EPS ** 2))
 
         # Correct for previous shift (it was only done on X, not on the gradients)
         # NOTE: This needs to be done AFTER computing sigma with shifted values.